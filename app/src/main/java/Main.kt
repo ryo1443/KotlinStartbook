18abbe34fe9021afaef1bf380588dba35764a552
ryo <ryo12271443@icloud.com>
2020-07-13 19:37:28 +0900

    4.2

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,13 +1,50 @@
 fun main() {
 
-    var count = 3
-    while (count-- > 0) { //マイナスマイナスが０より大きい
-        println("Hello!")
+    for (x in arrayOf(1, 2, 3)) { //イテレータを提供するオブジェクト（配列、リスト、レンジ）を繰り返す。
+        println(x)
+    }
+
+    val names = listOf("foo", "bar", "baz")
+    for (name in names) {
+        println(name)
+    }
+
+    for (i in 1..10 step 2) {
+        println(i)
     }
 
     //
-    do { //do-while文もサポートされている。
-        println("Hello")
-        val next = Math.random() < 0.5
-    } while (next)
+    var barIndex = -1
+    for (index in names.indices) { //indicesはリストなどの全インデックスをレンジとして返す
+        //"barが見つかったらループを抜ける
+        if (names[index] == "bar") {
+            barIndex = index
+            break
+        }
+    }
+    println(names[barIndex])
+
+    //
+    loop@ for (x in 1..10) { //ラベル付きのジャンプも可能。break@ラベル名 or continue@ラベル名でラベルへジャンプする。
+        for (y in 1..10) {
+            println(y)
+            if (y == 5) {
+                break@loop
+            }
+        }
+    }
+
+    //iterator実験。
+    for (item in MyIterable()) {
+        println(item)
+    }
+}
+
+class MyIterator { //この二つのメソッドを持つクラスがイテレータ。for文で繰り返し処理が行える。
+    operator fun hasNext(): Boolean = Math.random() < 0.75
+    operator fun next(): String = "Hello!"
+}
+
+class MyIterable {
+    operator fun iterator() = MyIterator()
 }
