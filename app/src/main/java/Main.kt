bf7a475d836d05d1d0077d2b3d1c6d3a82e485cb
ryo <ryo12271443@icloud.com>
2020-07-13 18:12:21 +0900

    3.1

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,18 +1,42 @@
 fun main() {
-    println(5 in 1..10)
-
-    val range: IntRange = 12..15
-    println(5 in range)
-
-    println(5 !in range)
+    if (true) {
+        println("Hello")
+    }
+    if (5 in 1..10) {
+        println("wawa")
+        println("hoho")
+    }
+    if (1 + 2 < 3)
+        println("hoge")
 
     //
-    println((1..5).toList())
+    val score = 50
+    if (score >= 60){
+        println("合格！")
+    } else {
+        println("不合格！")
+    }
 
-    println((1..5).reversed().toList()) //同じ
-    println((5 downTo 1).toList()) //同じ
+    val message = if (score >= 60) "合格！" else "不合格！"
+    println(message)
 
-    println((1..5 step 2).toList())
-    println((100 downTo 0 step 25).toList())
+    //
+    val x = if (true) { //ifやelseがブロックを取る場合、ブロック内で最後に評価される式が返される。今回は2
+        1
+        2
+    } else {
+        3
+    }
+    println(x)
 
+    //
+    val score2 = 75
+    val grade =
+        if (score2 >= 90) 'A'
+        else if (score2 >= 80) 'B'
+        else if (score2 >= 70) 'C'
+        else if (score2 >= 60) 'D'
+        else 'F'
+
+    println(grade)
 }
