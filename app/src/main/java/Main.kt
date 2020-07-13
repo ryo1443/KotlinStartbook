88a14418c20a0dc6fbe4df96640693c0f1a6833e
ryo <ryo12271443@icloud.com>
2020-07-13 14:22:17 +0900

    2.2

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,9 +1,20 @@
 fun main() {
-    val ints = arrayOfNulls<Int>(5) //インデックス数
+    val ints = arrayOfNulls<Int>(5) //インデックス数が引数
     println(ints.size)
 
     println(ints[0])
 
     ints[0] = 123
     println(ints[0])
+
+    //
+    val strs = arrayOf("red", "green", "blue") //要素が引数
+    println(strs[0])
+
+    //
+    val ints2: IntArray = intArrayOf(1, 2, 3)
+    println(ints2[0])
+
+    val chars: CharArray = charArrayOf('a', 'b')
+    println("${chars[0]}, ${chars[1]}")
 }
