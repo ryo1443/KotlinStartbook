c567078b048184c545d9593b1bb9d5ed8f6196c7
ryo <ryo12271443@icloud.com>
2020-07-16 01:33:14 +0900

    6.4

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -8,7 +8,18 @@ fun firstK(str: String): Int {
     return go(str, 0)
 }
 
+fun firstUpperCase(str: String): Int {
+    tailrec fun go(str: String, index: Int): Int =
+        when {
+            str.isEmpty() -> -1
+            str.first().isUpperCase() -> index
+            else -> go(str.drop(1), index + 1)
+        }
+    return go(str, 0)
+}
+
 fun main() { //高階関数・・・関数を引数として受け取ったり、返り値として返す関数。関数の抽象化が可能
     println(firstK("dcbehciboecbqcbqehcodKdndeqbeqyqu"))
+    println(firstUpperCase("dcbehciboecbqcbqehcodKdndeqbeqyqu"))
 }
 
