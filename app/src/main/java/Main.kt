ef285e8a47b22087c4c8b132dc5e827dc1d9138d
ryo <ryo12271443@icloud.com>
2020-07-16 12:28:41 +0900

    6.17

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,27 +1,13 @@
-fun foo(): Int {
-    val a = 1
-    val b = 2
-    return a + b
-}
+import android.util.Log
 
-//fun bar(): Int {
-//    val c = 3
-////    return a + c //aにアクセス不可でコンパイルエラー
-//}
-
-fun getCounter(): ()->Int { //関数オブジェクトを返す関数。
-    var count = 0
-    return {
-        count++
+fun log(debug: Boolean = true, message: () -> String) { //二つ目の引数が関数オブジェクトであることで、実行タイミングを遅らせることが出来る。
+    if (debug) {
+        println(message())
     }
 }
-fun main() { //クロージャ・・・コードを記述した時のスコープで変数が扱える関数オブジェクト
-    val counter1 = getCounter()
-    val counter2 = getCounter()
-    println(counter1()) //通常の関数ではあり得ない、スコープにアクセスした値の保持が出来ている。これがクロージャの最大のメリット。
-    println(counter1()) //関数内の値の保持
-    println(counter2())
-    println(counter1())
-    println(counter2())
+
+fun main() { //インライン関数・・・引数の関数オブジェクトが、コンパイル時にインライン展開される関数のこと
+    log { "このメッセージは出力される。"    }
+    log(false) { "このメッセージは出力されない"}
 }
 
