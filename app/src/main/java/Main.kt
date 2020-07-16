587b244504781f818815a4418eb2f45948120e9c
ryo <ryo12271443@icloud.com>
2020-07-16 11:53:50 +0900

    6.10

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,26 +1,18 @@
-fun firstK(str: String): Int {
-    fun isK(c: Char): Boolean = c == 'K'
-    return first(str, ::isK)
-}
 
-fun firstUpperCase(str: String): Int {
-    fun isUpperCase(c: Char): Boolean = c.isUpperCase()
-    return first(str, ::isUpperCase)
-}
+fun main() { //ラムダ式・・・関数オブジェクトを直接生成するコード
+    val square: (Int) -> Int = { i: Int ->
+        i * i //returnは不要。最後に評価される式が返ってくる。
+    }
+    println(square(5))
 
-//firstで上記二つのメソッドの共通部分を抽出している。
-fun first(str: String, predicate: (Char) -> Boolean): Int { //predicateはCharを引数にとって、Booleanを返す関数オブジェクト
-    tailrec fun go(str: String, index: Int): Int =
-        when {
-            str.isEmpty() -> -1
-            predicate(str.first()) -> index
-            else -> go(str.drop(1), index + 1)
-        }
-    return go(str, 0)
-}
+    val square2 = { i: Int -> //型推論が働く
+        i * i
+    }
+    println(square2(10))
 
-fun main() { //高階関数・・・関数を引数として受け取ったり、返り値として返す関数。関数の抽象化が可能
-    println(firstK("dcbehciboecbqcbqehcodKdndeqbeqyqu"))
-    println(firstUpperCase("dcbehciboecbqcbqehcodKdndeqbeqyqu"))
+    val square3: (Int) -> Int = { //引数が一つだけのラムダ式は暗黙の変数itが使える。
+        it * it
+    }
+
 }
 
