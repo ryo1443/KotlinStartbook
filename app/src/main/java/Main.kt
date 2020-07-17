948d1cbbdcd2aca195c2e214b97cd594863a8428
ryo <ryo12271443@icloud.com>
2020-07-17 11:50:30 +0900

    7.4

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,11 +1,42 @@
 interface Bucket {//オブジェクト式だとバケツはデータ型が無いので、名前による参照が出来ず、使い物にならない。interfaceで型を定義。
     fun fill() //インターフェースによって、Bucketが持つべきメソッドを定義。
     fun drainAway()
-    fun pourTo()
+    fun pourTo(that: Bucket)
 
     fun getCapacity(): Int
     fun getQuantity(): Int
-    fun setQuantity(quantity: Int): Int
+    fun setQuantity(quantity: Int)
+}
+
+fun createBucket(capacity: Int) : Bucket = object : Bucket {
+    var _quantity: Int = 0
+
+    override fun fill() { //インターフェースのメソッドを実装するには必ずoverrideの修飾子を付ける
+        setQuantity(getCapacity())
+    }
+
+    override fun drainAway() {
+        setQuantity(0)
+    }
+
+    override fun pourTo(that: Bucket) {
+        val thatVacuity = that.getCapacity() - that.getQuantity()
+        if (getQuantity() <= thatVacuity) {
+            that.setQuantity(that.getQuantity() + getQuantity())
+            drainAway()
+        } else {
+            that.fill()
+            setQuantity(getQuantity() - thatVacuity)
+        }
+    }
+
+    override fun getCapacity(): Int = capacity
+
+    override fun getQuantity(): Int = _quantity
+
+    override fun setQuantity(quantity: Int): Int {
+        _quantity = quantity
+    }
 }
 fun main() { //オブジェクトを生成するにはobjectを使う。オブジェクト内の関数的なのはメソッドという。
     val bucket = object : Bucket { //インターフェースを実装
