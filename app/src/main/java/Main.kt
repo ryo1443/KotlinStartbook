3c81733649eb113d0b923ec5a33b0354f0117639
ryo <ryo12271443@icloud.com>
2020-07-18 22:48:57 +0900

    8.5

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,11 +1,18 @@
-class Greeter {
-    fun greet(name: String) { //メソッド・・・オブジェクトを操作するための関数的なモノ
-        println("Hello!, $name!")
-    }
+class Person {
+    var name: String = "" //プロパティ・・・オブジェクトの特性や性質を表すデータをやり取りするための窓口。
+    var age: Int = 0      //プロパティを介して、オブジェクトの状態を知ったり変更したりすることが出来る。
 }
 
 fun main() { //クラス・・・オブジェクトの設計図。オブジェクトを量産できる。逆に言うと、クラスはそれまででしかない。
-    val greeter = Greeter()
-    greeter.greet("Kotlin") //「Hello!Kotlin!」と出力)
+    val hanako = Person()
+
+    println(hanako.name) //「」を出力
+    println(hanako.age)  //「0」を出力
+
+    hanako.name = "はなこ"
+    hanako.age = 25
+
+    println(hanako.name) //「はなこ」を出力
+    println(hanako.age)  //「25」を出力
 }
 
