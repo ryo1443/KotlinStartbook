659be9d3f71017b4853539e1b0471c51e48137ed
ryo <ryo12271443@icloud.com>
2020-07-18 22:33:32 +0900

    8.3

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,7 +1,11 @@
-class MyClass()
+class Greeter {
+    fun greet(name: String) { //メソッド・・・オブジェクトを操作するための関数的なモノ
+        println("Hello!, $name!")
+    }
+}
 
 fun main() { //クラス・・・オブジェクトの設計図。オブジェクトを量産できる。逆に言うと、クラスはそれまででしかない。
-    val myClass: MyClass = MyClass() //変数myClassの型はMyClass。クラスを定義すると、同名の型が定義され、他のクラスにより生成されたオブジェクトと混同する心配がなくなる。
-    println(myClass) //MyClass@2503dbd3を返す
+    val greeter = Greeter()
+    greeter.greet("Kotlin") //「Hello!Kotlin!」と出力)
 }
 
