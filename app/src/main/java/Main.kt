165970f27af7867ddab9b9495bb9b1024f8f1a8d
ryo <ryo12271443@icloud.com>
2020-07-19 23:44:32 +0900

    8.8

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,7 +1,11 @@
 class Person {
     var name: String = "" //バッキングフィールド・・・プロパティに自動生成される。プロパティのみアクセス可能。
-    var age: Int = 0
+        set(value) { //カスタムセッター・・・引数はvalueが一般的。fieldというバッキングフィールドを表す暗黙の変数にvalueを格納している。
+            println("${value}がセットされました") //ゲッターやセッターにカスタムロジックを入れたい場合は、このようにして「バッキング・フィールド」を使う。
+            field = value
+        }
 
+    var age: Int = 0
     val nameLength: Int //カスタムゲッター・・・バッキングフィールドを持たない。
         get() = this.name.length //返り値の型とreturnの省略バージョン
 
@@ -10,8 +14,7 @@ class Person {
 fun main() { //クラス・・・オブジェクトの設計図。オブジェクトを量産できる。逆に言うと、クラスはそれまででしかない。
     val hanako = Person()
 
-    println(hanako.name) //「」を出力
-    hanako.name = "はなこ"
-    println(hanako.nameLength) //「3」を出力
+    hanako.name = "はなこ" //「はなこがセットされました」を出力
+    println(hanako.name) //「はなこ」を出力
 }
 
