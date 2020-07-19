d90ef784a2be9cf1d4635cdab93c7ced07dcc948
ryo <ryo12271443@icloud.com>
2020-07-19 23:38:50 +0900

    8.7

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -3,9 +3,8 @@ class Person {
     var age: Int = 0
 
     val nameLength: Int //カスタムゲッター・・・バッキングフィールドを持たない。
-        get(): Int {
-            return this.name.length
-        }
+        get() = this.name.length //返り値の型とreturnの省略バージョン
+
 }
 
 fun main() { //クラス・・・オブジェクトの設計図。オブジェクトを量産できる。逆に言うと、クラスはそれまででしかない。
