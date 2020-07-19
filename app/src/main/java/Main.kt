50ac803c20f7125737c1c360e29128227d65c34b
ryo <ryo12271443@icloud.com>
2020-07-20 00:13:57 +0900

    8.12

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,10 +1,12 @@
 class Rational (val numerator: Int, val denominator: Int) { //valやvarを付けると、そのままプロパティとして使える。
-
+    constructor(numerator: Int) : this(numerator, 1) //セカンダリコンストラクタ・・・プライマリコンストラクタとは別のコンストラクタ
 }
 
 fun main() {
     val half = Rational(1, 2)
-    println(half.numerator) //「１」
-    println(half.denominator) //「２」
+    println("${half.numerator} / ${half.denominator}") //「1/2」
+
+    val five = Rational(5)
+    println("${five.numerator} / ${five.denominator}") //「5/1」
 }
 
