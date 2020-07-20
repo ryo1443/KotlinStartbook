64789f30d26548d35551162c2b8260a6375986ec
ryo <ryo12271443@icloud.com>
2020-07-21 01:12:10 +0900

    8.19

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,12 +1,17 @@
-class Rational (val numerator: Int, val denominator: Int = 1) { //イニシャライザ・・・インスタンス生成の際に実施しておきたい処理（初期化）をクラス内に定義することが出来る。
-    init {
-        //要求に反した場合、例外をスローする標準ライブラリ関数
-        require(denominator != 0)
-    }
-}
 
-fun main() {
-    Rational(1, 1) //OK
-    Rational(1, 0) //エラー
+fun countWords(s: String): Int =
+    s.split("""\s+""".toRegex()).size //文字列を空白文字で分けた配列の大きさを返す
+
+fun String.countWords2(): Int =
+    this.split("""\s+""".toRegex()).size //拡張関数バージョン。左辺がそのままmainで使われている。String→this
+
+val String.wordsCount: Int
+    get() = split("""\s+""".toRegex()).size //拡張プロパティバージョン。カスタムゲッターの中で、単語数の処理を行っている。
+
+//拡張関数や拡張プロパティをまとめて、エクステンションと呼ぶ
+fun main() { //拡張関数・・・既存のクラスに対して、その定義には触れずにメソッドを追加する仕組み
+    println(countWords("I like Kotlin")) //「３」
+    println("I like Kotlin".countWords2())
+    println("I like Kotlin".wordsCount)
 }
 
