86aaabfc233f2869c8e39d3d1fa59b8c272cf44c
ryo <ryo12271443@icloud.com>
2020-07-23 01:22:43 +0900

    10.6

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,14 +1,16 @@
-interface Foo {
-    fun execute()
-}
-
-open class Superclass {
-    open fun execute() {
-        println("Superclass")
+interface Hoge { //インターフェースは実装の中身を持つことが出来る
+    fun execute() {
+        println("Hoge")
     }
 }
 
-class FooSubclass : Superclass(), Foo //文字通りスーパークラスで実装されているので、問題は生じない
+interface Fuga {
+    fun execute() {
+        println("Fuga")
+    }
+}
+
+class HogeFuga : Hoge, Fuga //インターフェースで実装が提供されているexecuteメソッドについて、どちらのものを使うか曖昧なのでエラーが生じる
 
 fun main() { //インターフェース・・・実装するオブジェクトにおけるプロトコル（取り決め）を表現する。
 
