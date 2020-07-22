714deceb7545dbf233fd6d6593de5913da003586
ryo <ryo12271443@icloud.com>
2020-07-22 12:58:43 +0900

    9.7

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,20 +1,22 @@
-open class Person(open val name: String) { //継承元、スーパークラス。openを付けないと継承不可。
-    open fun introduceMyself() {
-        println("I am $name")
+abstract class Greeter(val target: String) {
+    abstract fun sayHello()
+}
+
+class EnglishGreeter(target: String) : Greeter(target) {
+    override fun sayHello() {
+        println("Hello, $target")
     }
 }
 
-class Student(override val name: String, val id: Long): Person(name) {//プロパティもオーバーライドできる。
-    override fun introduceMyself() {
-    println("--自己紹介ここから--")
-    super.introduceMyself() //スーパーメソッド
-    println("--自己紹介ここまで--")
+class JapaneseGreeter(target: String) : Greeter(target) {
+    override fun sayHello() {
+        println("こんにちは、$target")
     }
 }
 
-fun main() { //継承・・・既存のクラスの機能を拡張することが出来る
-    val person: Person = Student("たろう", 456) //スーパータイプのインスタンス。実態はStudentだが、型はPerson
-    person.introduceMyself()
-    person.id //エラー
+fun main() { //抽象クラス・・・インターフェース的な役割。インターフェースとの違いは実装できるか否か。
+    EnglishGreeter("Kotlin").sayHello()
+
+    JapaneseGreeter("Java").sayHello()
 }
 
