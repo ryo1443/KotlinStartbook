deee7a963a22f92261d7cb105fcd5222c8ea1352
ryo <ryo12271443@icloud.com>
2020-07-22 12:43:18 +0900

    9.3

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,18 +1,19 @@
 open class Person(val name: String) { //継承元、スーパークラス。openを付けないと継承不可。
-    fun introduceMyself() {
+    open fun introduceMyself() {
         println("I am $name")
     }
 }
 
-class Student(name: String, val id: Long): Person(name) //継承先、サブクラス。nameにはvalやvarは必要ない。（定義済みだから）
+class Student(name: String, val id: Long): Person(name) {//継承先、サブクラス。nameにはvalやvarは必要ない。（定義済みだから）
+    override fun introduceMyself() {
+    println("I am $name(id = $id)")
+    }
+}
 
 fun main() { //継承・・・既存のクラスの機能を拡張することが出来る
-    val person: Person = Person("ゆたか")
-    person.introduceMyself() //「I am ゆたか」
-
     val student: Student = Student("くみこ", 123)
     println(student.name)
     println(student.id)
-    student.introduceMyself() //スーパークラスのメンバが使える
+    student.introduceMyself() //オーバーライドで上書きが可能
 }
 
