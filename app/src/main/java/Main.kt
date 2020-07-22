937f1905255293af5d9fe5a53dfc01135763625a
ryo <ryo12271443@icloud.com>
2020-07-23 01:24:46 +0900

    10.7

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -10,8 +10,12 @@ interface Fuga {
     }
 }
 
-class HogeFuga : Hoge, Fuga //インターフェースで実装が提供されているexecuteメソッドについて、どちらのものを使うか曖昧なのでエラーが生じる
-
+class HogeFuga : Hoge, Fuga {
+    //インターフェースで実装が提供されているexecuteメソッドについて、どちらのものを使うか曖昧なのでエラーが生じる
+    override fun execute() {
+        super<Hoge>.execute() //super<インターフェースorクラス名>.メソッド()で、どれを実装するか指定できる。
+    }
+}
 fun main() { //インターフェース・・・実装するオブジェクトにおけるプロトコル（取り決め）を表現する。
 
 }
