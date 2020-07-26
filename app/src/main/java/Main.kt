5c081a0a5135102088950208ec5cd06a6f0ed2b8
ryo <ryo12271443@icloud.com>
2020-07-27 03:14:59 +0900

    10.10

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,20 +1,40 @@
-interface Foo { //インターフェースの継承・・・インターフェースも継承が出来る。その場合は継承元のメンバを受け継ぐ
-    fun aaa()
-    fun bbb()
+interface Greeter {
+    fun sayHello(target: String)
+    fun sayHello()
 }
 
-interface Bar: Foo {
-    override fun aaa() {} //継承したらオーバーライドしなければならない
-    fun ccc()
+open class JapaneseGreeter() : Greeter {
+    override fun sayHello(target: String) {
+        println("こんにちは、$target!")
+    }
+
+    override fun sayHello() {
+        println("こんにちは、匿名さん!")
+    }
 }
 
-class Baz: Bar {
-    override fun bbb() {}
+class JapaneseGreeterWithRecording() : JapaneseGreeter() {
+    private val _targets: MutableSet<String> = mutableSetOf() //非公開プロパティ
 
-    override fun ccc() {}
+    val targets: Set<String> //カプセル化を行っている
+        get() = _targets
+
+    override fun sayHello(target: String) { //sayHelloの内容を記録するだけ
+        _targets += target
+        super.sayHello(target)
+    }
 }
-
 fun main() { //インターフェース・・・実装するオブジェクトにおけるプロトコル（取り決め）を表現する。
+    val greeter = JapaneseGreeter()
+    greeter.sayHello()
+    greeter.sayHello("Kotlin")
 
+    val greeter2 = JapaneseGreeterWithRecording()
+    greeter2.sayHello("うらがみ")
+    greeter2.sayHello("がくぞ")
+    println(greeter2.targets)
+    greeter2.sayHello("***")
+    greeter2.sayHello()
+    println(greeter2.targets)
 }
 
