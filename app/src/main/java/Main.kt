5ee38487ba9f6aa3f7f008c63ed5503e71cb7233
ryo <ryo12271443@icloud.com>
2020-07-27 02:34:40 +0900

    10.8

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,21 +1,19 @@
-interface Hoge { //インターフェースは実装の中身を持つことが出来る
-    fun execute() {
-        println("Hoge")
-    }
+interface Foo { //インターフェースの継承・・・インターフェースも継承が出来る。その場合は継承元のメンバを受け継ぐ
+    fun aaa()
+    fun bbb()
 }
 
-interface Fuga {
-    fun execute() {
-        println("Fuga")
-    }
+interface Bar: Foo {
+    override fun aaa() {} //継承したらオーバーライドしなければならない
+    fun ccc()
 }
 
-class HogeFuga : Hoge, Fuga {
-    //インターフェースで実装が提供されているexecuteメソッドについて、どちらのものを使うか曖昧なのでエラーが生じる
-    override fun execute() {
-        super<Hoge>.execute() //super<インターフェースorクラス名>.メソッド()で、どれを実装するか指定できる。
-    }
+class Baz: Bar {
+    override fun bbb() {}
+
+    override fun ccc() {}
 }
+
 fun main() { //インターフェース・・・実装するオブジェクトにおけるプロトコル（取り決め）を表現する。
 
 }
