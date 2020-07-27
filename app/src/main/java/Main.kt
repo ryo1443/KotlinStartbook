73e3f8f747f11222c1846bc767a0cbbdf2d304b3
ryo <ryo12271443@icloud.com>
2020-07-28 03:41:40 +0900

    11.2

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,5 +1,8 @@
 class Container(var value: Any) //オブジェクトの型はAny
 
+class IntContainer(var value: Int)
+class StrContainer(var value: String) //このような型を定義しても良いが、他の型に対応できない
+
 fun main() { //ジェネリクス
     val intContainer = Container(123)
     val i: Int = intContainer.value as Int //強制的に型を変える処理をキャストという
