ecc02115c9d767282330817097bfb2a6190111ff
ryo <ryo12271443@icloud.com>
2020-07-28 03:33:47 +0900

    10.12

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -13,34 +13,25 @@ open class JapaneseGreeter() : Greeter {
     }
 }
 
-class JapaneseGreeterWithRecording() : Greeter {
-    private val greeter: Greeter = JapaneseGreeter()
+class GreeterWithRecording(private val greeter: Greeter) : Greeter by greeter { //クラスデリゲーション・・・インターフェースGreeterを実装するが、オーバーライドしてないメンバはgreeterに委譲する
+    //sayHello()は実装しなくても、自動的にgreeterが実装してくれている。
+    private val _targets: MutableSet<String> = mutableSetOf()
 
-    private val _targets: MutableSet<String> = mutableSetOf() //非公開プロパティ
-
-    val targets: Set<String> //カプセル化を行っている
+    val targets: Set<String>
         get() = _targets
-
-    override fun sayHello() { //委譲・・・あるオブジェクトの仕事を、別のオブジェクトに任せる手法
-        greeter.sayHello()
-    }
 
     override fun sayHello(target: String) {
         _targets += target
         greeter.sayHello(target)
     }
 }
-fun main() { //インターフェース・・・実装するオブジェクトにおけるプロトコル（取り決め）を表現する。
-    val greeter = JapaneseGreeter()
-    greeter.sayHello()
-    greeter.sayHello("Kotlin")
 
-    val greeter2 = JapaneseGreeterWithRecording()
-    greeter2.sayHello("うらがみ")
-    greeter2.sayHello("がくぞ")
-    println(greeter2.targets)
-    greeter2.sayHello("***")
-    greeter2.sayHello()
-    println(greeter2.targets)
+fun main() { //委譲・・・具象クラスの全てを継承するのではなく、抽象クラスを継承し一部を具象クラスから引っ張ってくる
+    val japanese = JapaneseGreeter()
+    val greeter = GreeterWithRecording(japanese) //自由に実装を選ぶことが出来る。
+    greeter.sayHello()
+    greeter.sayHello("委譲")
+    println(greeter.targets)
+
 }
 
