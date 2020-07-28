fb0ac6cce96d0d65d8b3e989e4432a0c099f39ef
ryo <ryo12271443@icloud.com>
2020-07-28 19:34:59 +0900

    11.6 modfied

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -3,11 +3,11 @@ interface Fuga
 class Foo<T>
 class Bar<T: Hoge>
 
-fun main() { //ジェネリ
+fun main() { //ジェネリック制約・・・型パラメータに対して、制約を設けることが出来る。
     Foo<Hoge>()
     Foo<Fuga>()
 
     Bar<Hoge>()
-    Bar<Fuga>()
+    Bar<Fuga>() //BarはHogeのサブタイプでなければならない。
 }
 
