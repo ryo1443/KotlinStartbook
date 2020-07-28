c4c2b658be14d28be3ad11eb3d1e37a52b455ecb
ryo <ryo12271443@icloud.com>
2020-07-28 19:28:05 +0900

    11.5

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,13 +1,14 @@
-import android.support.v4.app.INotificationSideChannel
+class Container<T>(var value: T)
 
-class Container<T>(var value: T) //<T>という型パラメータをとるジェネリクスクラス
+val <T> T.string: String
+    get() = toString()
 
-fun main() { //ジェネリクス・・・仮決めの型で柔軟にインスタンスを生成できる機構
-    val intContainer: Container<Int> = Container<Int>(123) //型引数として<Int>を渡す
-    val i: Int = intContainer.value
-    println(i)
+fun <T> box(value: T): Container<T> =
+    Container(value)
 
-    val strContainer = Container<String>("Hello")
-    println(strContainer.value.toUpperCase())
+
+fun main() { //関数やプロパティにもジェネリック関数やジェネリックプロパティとして適用できる。
+    val container: Container<Int> = box(5)
+    println(container.string)
 }
 
