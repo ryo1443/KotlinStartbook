6e202ce8ee887e1017c4a71c8a6d5d94151b0584
ryo <ryo12271443@icloud.com>
2020-07-28 19:37:14 +0900

    11.7

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,13 +1,10 @@
 interface Hoge
 interface Fuga
-class Foo<T>
-class Bar<T: Hoge>
+interface Piyo: Hoge, Fuga
+
+class Baz<T> where T : Hoge, T : Fuga //whereを用いることで、複数の上限境界（制約）を設定できる
 
 fun main() { //ジェネリック制約・・・型パラメータに対して、制約を設けることが出来る。
-    Foo<Hoge>()
-    Foo<Fuga>()
-
-    Bar<Hoge>()
-    Bar<Fuga>() //BarはHogeのサブタイプでなければならない。
+    Baz<Piyo>() //OK
 }
 
