3ea7dd1ad22b7ef2ca2e80571f59d20113278a08
ryo <ryo12271443@icloud.com>
2020-07-28 21:40:24 +0900

    11.16

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,14 +1,13 @@
-class Container<T>(var value: T) {
-    fun copyTo(to: Container<in T>) { //反変にはinを使う。
-        to.value = value
-    }
+class Container<out T>(val value: T) //Tに対して共変であるContainer
+
+fun show(container: Container<Any>) { //宣言場所変位指定によって、逐一型投影を書く手間が減る。
+    println(container.toString())
+    println(container.hashCode())
+    println(container.value)
 }
 
-fun main() { //反変・・・StringがCharSequenceのスーパータイプになるような事
-    //inは入力専用outは出力専用無しだと入出力に対応と覚えればよい
-    val a: Container<Int> = Container(15)
-    val b: Container<Number> = Container(0)
-    a.copyTo(b) //bの値をaの値に変えている。このように、inは値の書き換えが可能
-    println(b.value) //15
+fun main() { //宣言場所変位指定・・・クラスの宣言場所で変位を指定できる
+    val a: Container<String> = Container("Hello")
+    show(a)
 }
 
