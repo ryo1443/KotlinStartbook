46336cd9800b7a4ab8aa0340df97c2f361e8d8c2
ryo <ryo12271443@icloud.com>
2020-07-30 21:13:20 +0900

    12.13

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,23 +1,8 @@
-import java.lang.AssertionError
+fun main() { //安全キャスト・・・asを用いたダウンキャストをnull安全にしたもの。as?を使う
+    val str: Any = "本当は文字列"
+    println(str as String)
+//    println(str as Int) //エラー
 
-fun main() { //エルビス演算子・・・nullでなければそれを使用し、nullならデフォルト値を使うといった時に使う
-    val foo: String? = "Hello"
-    println((if (foo != null) foo else "default").toUpperCase())
-
-    val hoge: String? = null
-    println((if(hoge != null) hoge else "default"))
-
-    //エルビス演算子を使うと以下のようになる
-    val foo2: String? = "Hello"
-    println((foo2 ?: "default").toUpperCase())
-
-    val hoge2: String? = null
-    println(hoge ?: "default")
-
-    //例外をスローさせるなら以下
-    val aaa: String? = null
-    println(aaa ?: throw AssertionError())
-
-
+    println(str as? Int) //null
 }
 
