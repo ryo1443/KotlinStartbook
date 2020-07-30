7349c0e683f848a2b0f1db66e0caabcb16892dd3
ryo <ryo12271443@icloud.com>
2020-07-30 18:10:01 +0900

    12.11

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,9 +1,11 @@
+fun square(i: Int) = println(i*i)
 
 fun main() { //安全呼び出し・・・nullの時はnullを返し、null出ない時は何かしら処理を行わせる為の糖衣構文
-    val a: Int? = null
-    val aInc: Int? = a?.inc() //aがnullの時はnullを返し、そうでないときはinc()が呼び出される。返り値はInt?
+    val a: Int? = 5
+    val aSquare = //オブジェクトのメンバ呼び出し以外の場合（今回）、squareはNotNullな引数を取るのでNullableを渡す時ひと手間。
+        if (a != null) square(a)
+        else null
 
-    println(aInc)
-
+    val aSquare2 = a?.let { square(it) } //letを使えば上のような場合でも簡潔に書ける
 }
 
