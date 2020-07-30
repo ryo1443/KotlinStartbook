452cd5381d9d0b6a67107d3b63ca2894446e3447
ryo <ryo12271443@icloud.com>
2020-07-30 18:16:36 +0900

    12.12

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,11 +1,23 @@
-fun square(i: Int) = println(i*i)
+import java.lang.AssertionError
 
-fun main() { //安全呼び出し・・・nullの時はnullを返し、null出ない時は何かしら処理を行わせる為の糖衣構文
-    val a: Int? = 5
-    val aSquare = //オブジェクトのメンバ呼び出し以外の場合（今回）、squareはNotNullな引数を取るのでNullableを渡す時ひと手間。
-        if (a != null) square(a)
-        else null
+fun main() { //エルビス演算子・・・nullでなければそれを使用し、nullならデフォルト値を使うといった時に使う
+    val foo: String? = "Hello"
+    println((if (foo != null) foo else "default").toUpperCase())
 
-    val aSquare2 = a?.let { square(it) } //letを使えば上のような場合でも簡潔に書ける
+    val hoge: String? = null
+    println((if(hoge != null) hoge else "default"))
+
+    //エルビス演算子を使うと以下のようになる
+    val foo2: String? = "Hello"
+    println((foo2 ?: "default").toUpperCase())
+
+    val hoge2: String? = null
+    println(hoge ?: "default")
+
+    //例外をスローさせるなら以下
+    val aaa: String? = null
+    println(aaa ?: throw AssertionError())
+
+
 }
 
