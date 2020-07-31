253c55ae812d7d7be1b52bcfb26db31fc59a5c49
ryo <ryo12271443@icloud.com>
2020-08-01 01:16:26 +0900

    companion object

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,20 +1,13 @@
-interface Greeter {
-    fun greet(name: String)
-}
-
-object JapaneseGreeter: Greeter { //クラスを継承したリ、インターフェースを実装したリ出来る。
-    override fun greet(name: String) {
-        println("こんにちは、$name さん")
+class User(val id: Long, val name: String) {
+    companion object Pool { //Poolという名前のcompanion object
+        val DUMMY = User(0, "dummy")
     }
 }
 
-fun main() { //オブジェクト宣言・・・シングルトン（唯一のインスタンスが存在するようなパターン）なクラスを定義する
-    JapaneseGreeter.greet("たろう")
+fun main() { //コンパニオンオブジェクト・・・シングルトンオブジェクトをクラス内に定義する。一つのクラスに一つだけ
+    val dummy = User.Pool.DUMMY
+    println("${dummy.id}, ${dummy.name}")
 
-    val greeter: JapaneseGreeter = JapaneseGreeter
-    greeter.greet("次郎")
-
-    val greeter2: JapaneseGreeter = JapaneseGreeter
-    greeter2.greet("さぶろう")
+    println(User.DUMMY.name) //Poolを経由せずとも直接メンバにアクセスできる
 }
 
