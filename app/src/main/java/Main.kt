3d38af8521f97395e7a00a93c984cd404149f341
ryo <ryo12271443@icloud.com>
2020-07-31 19:00:49 +0900

    invoke

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,10 +1,11 @@
-class MyInt(val value: Int) {
-    operator fun times(that: MyInt): MyInt =
-        MyInt(value * that.value)
+class Service { //invokeを持つオブジェクトはオブジェクト自身が関数のような役割を果たす
+    operator fun invoke(): Char = 'A'
+    operator fun invoke(c: Char) = c
 }
 
 fun main() { //演算子オーバーロード・・・あるメソッドを対応する演算子で呼び出せるようにする仕組み。
-    val product = MyInt(3) * MyInt(5)
-    println(product.value)
+    val service = Service()
+    println(service())
+    println(service('B'))
 }
 
