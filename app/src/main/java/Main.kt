9207acf255e2635f20f944de739e8f421f3f59b8
ryo <ryo12271443@icloud.com>
2020-07-31 19:13:23 +0900

    dataclass

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,11 +1,19 @@
-class Service { //invokeを持つオブジェクトはオブジェクト自身が関数のような役割を果たす
-    operator fun invoke(): Char = 'A'
-    operator fun invoke(c: Char) = c
-}
+class User(val id: Long, val name: String) //toStringなどを実装しないと不便
 
-fun main() { //演算子オーバーロード・・・あるメソッドを対応する演算子で呼び出せるようにする仕組み。
-    val service = Service()
-    println(service())
-    println(service('B'))
+data class User2(val id: Long, val name: String) //Userのデータクラス版
+
+fun main() { //dataクラス・・・データを持つことに特化したクラス。
+    println(User(1, "Taro") == User(1, "Taro")) //同じであるはずなのにfalse
+    println(User(2, "Hanako"))
+
+    println(User2(1, "Taro") == User2(1, "Taro")) //期待通りtrue
+    println(User2(2, "Hanako")) //内容がちゃんと見れる
+
+    val taro = User2(1, "Taro")
+    println(taro)
+
+    val newTaro = taro.copy(id = 112233) //レシーバとなるオブジェクトのコピーを生成する。
+    println(newTaro)
+    println(taro)
 }
 
